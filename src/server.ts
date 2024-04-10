d5e230d62e0d72f76faa747d0f31eebc96f12410
Utee <utibeabasiekong2@gmail.com>
2024-04-10 15:59:45 +0100

    update: add newsletter component to the server

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,6 @@
 import { DolphFactory } from "@dolphjs/dolph";
 import { AccountComponent } from "./components/account/account.component";
+import { NewsletterComponent } from "./components/newsletter/newsletter.component";
 
-const dolph = new DolphFactory([AccountComponent]);
+const dolph = new DolphFactory([AccountComponent, NewsletterComponent]);
 dolph.start();
