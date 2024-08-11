4c0c9dfe75ad6b601c40d816f4d8d845f9d59735
Utee <utibeabasiekong2@gmail.com>
2024-08-11 16:47:54 +0100

    update: dolph version

--- src/components/newsletter/newsletter.service.ts
+++ src/components/newsletter/newsletter.service.ts
@@ -33,7 +33,7 @@ export class NewsletterService extends DolphServiceHandler<Dolph> {
     return sendSubscribedMail(
       dto.email,
       "https://dolphjs.com",
-      "https://docs.dolphjs.com"
+      `http://13.40.31.183:3300/newsletter/unsubscribe/${user.email}`
     );
   }
 
