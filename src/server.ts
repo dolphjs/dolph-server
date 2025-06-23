194a0fe911c9033cbba28e615c2c210819a3afe0
Christopher Egbaaibon <christopheregbaaibon@gmail.com>
2025-06-23 02:02:13 +0100

    update: upgrade dolph dependency and enhance CORS configuration

--- src/server.ts
+++ src/server.ts
@@ -3,7 +3,15 @@ import { AccountComponent } from "./components/account/account.component";
 import { NewsletterComponent } from "./components/newsletter/newsletter.component";
 import path from "path";
 import helmet from "helmet";
+import cors from "cors";
 
+const corsOptions = {
+  origin: ["http://localhost:4200", "https://dolphjs.com"],
+  methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+};
+
+middlewareRegistry.register(cors(corsOptions));
 middlewareRegistry.register(helmet());
 
 MVCAdapter.setViewEngine("pug");
